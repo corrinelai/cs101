d90aa71f924a8a644bb1d9274d94a21ead05c138
corrinelai <100830222+corrinelai@users.noreply.github.com>
2023-01-04 01:21:52 +0800

    Create main9.c

---
+++ HW8/main9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+int main(){  
+    int i,j;
+    for (i=0;i<100,j!=3;i++,j++){
+        printf("i=%d\n",i);
+    }
+    printf("會停 因為當j等於3時就不滿足迴圈條件而停止迴圈");
+    return 0;
+} 
